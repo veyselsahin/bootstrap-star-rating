912b48f7aa78d9c3c07eb013609ef01643d69510
Veysel Şahin <v_sahin@anadolu.edu.tr>
2016-06-11 17:53:40 +0300

    Create star-rating_locale_tr-TR.js

---
+++ js/star-rating_locale_tr-TR.js
@@ -0,0 +1,31 @@
+/*!
+ * Star Rating <LANG> Translations
+ *
+ * This file must be loaded after 'star-rating.js'. Patterns in braces '{}', or
+ * any HTML markup tags in the messages must not be converted or translated.
+ *
+ * @see http://github.com/kartik-v/bootstrap-star-rating
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+(function ($) {
+    "use strict";
+    $.fn.ratingLocales['<LANG>'] = {
+        defaultCaption: '{rating} Stars',
+        starCaptions: {
+            0.5: 'Yarım Yıldız',
+            1: 'Bir Yıldız',
+            1.5: 'Bir & Buçuk Yıldız',
+            2: 'İki Yıldız',
+            2.5: 'İki & Buçuk Yıldız',
+            3: 'Üç Yıldız',
+            3.5: 'Üç & Buçuk Yıldız',
+            4: 'Dört Yıldız',
+            4.5: 'Dört & Buçuk Yıldız',
+            5: 'Beş Yıldız'
+        },
+        clearButtonTitle: 'Temizle',
+        clearCaption: 'Oylanmadı'
+    };
+})(window.jQuery);
